390719fd192ade0d93eb17801422aea074c6bfd4
Korrapati Jaswanth <132824814+Jashfg@users.noreply.github.com>
2024-05-29 16:52:30 +0530

    Added remainder

---
+++ mycalc (1).py
@@ -0,0 +1,27 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    if b==0:
+        print("division is not possible")
+    else:
+        print(a/b)
+
+pi=3.14
+z=2.7
+
+def remainder(a,b):
+    return a%b
+
+
+def some():
+    print("hello")
+    
+print("this is module")
+some()
